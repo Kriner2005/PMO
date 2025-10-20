e737fb8961dbea3d1af5a1d8b830f6eca35fb2c5
keiner <keiner.alberto@uptc.edu.co>
2025-10-19 19:12:54 -0500

    faltante hilo y timers

--- src/uptc/edu/co/utilities/Utilities.java
+++ src/uptc/edu/co/utilities/Utilities.java
@@ -1,9 +1,9 @@
 package uptc.edu.co.utilities;
 
 public class Utilities {
+
     public static final String DATA_FOLDER = "data/";
-    public static final String USERS_FILE = DATA_FOLDER + "users.json";
-    public static final String SESSIONS_FILE = DATA_FOLDER + "sessions.json";
-    public static final String HISTORIES_FILE = DATA_FOLDER + "histories.json";
-    public static final String SETTINGS_FILE = DATA_FOLDER + "settings.json";
+    public static final String USERS_FILE = DATA_FOLDER + "users/users.json";
+    public static final String HISTORIES = DATA_FOLDER + "usersHistory/";
+
 }
